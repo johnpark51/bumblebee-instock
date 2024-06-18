fa02625c3c9c9d6030323d8d546540845befc4b5
Jason Korol <ejkorol@gmail.com>
2024-06-18 11:28:38 -0400

    import WarehouseList into WarehousePage

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,11 +1,15 @@
+/* STYLES */
 import "./WarehousePage.scss";
+
+/* COMPONENTS */
+import WarehouseList from "@/components/WarehouseList/WarehouseList";
 
 function WarehousePage () {
   return (
-    <>
-      ...
-    </>
-  )
+    <main>
+      <WarehouseList />
+    </main>
+  );
 };
 
 export default WarehousePage;
